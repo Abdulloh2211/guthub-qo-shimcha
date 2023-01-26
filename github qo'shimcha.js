de2be00d203df26f76cbcc30ef56a90093116582
Abdulloh2211 <abdulloh5670089@gmail.com>
2023-01-26 23:34:42 +0500

    qoshimcha github

---
+++ github qo'shimcha.js
@@ -0,0 +1,12 @@
+// Origin buni biz xar xil boshqa ismlar bilan nomlasak bo'ladi va bu bizga 1 kata papkaga yana keyinxhalik information qo'shish uchun ishlaydi
+// Endi biz git push origin master emas balkim boshqa ism bersak bo'ladi masalan git push g9 origin
+
+// agar biz git push -u origini master bersak endi biz yanegi safar git push desak o'zi avtomatik tarzda bizga git push origin master qilib beradi
+
+// birinchi bo'lib biz git init qilib .git fayl ochib olishimizdan maqsad bu qila yotgan narsalarimizni hech qachon srazu githubga joylab keta olmaymiz.
+
+// branchdi o'chirish uchun biz git branch --delete branchname yozamiz
+
+// bizda stack bor va heap bor -bular ikkalasi biz yozgan js kodlariga aloqadirdir .Endi shunda agar biz ikkita bir xil primitivelardi solishtirsak unda true chiqadi va ikkita bir xil non-primitive lardi solishtirsak false beradi chunki 
+// primlar pass by value non-primlar esa pass by reference ,chunki bu bazada save bo'lyap turganda value srazu qatorma qator chiqarib beradi 
+// lekin non-primlar esa bizga link yaratib beradi va solishtirib turganda ikkita qiymat ikkalsi ham boshqa link bo'lgani uchun false beradi yana linklar hech qachon bir-biriga to'g'ri kelmaydi.
